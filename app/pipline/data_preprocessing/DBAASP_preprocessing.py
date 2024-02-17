3623a7bb3eb7e6ce6fdbe0fa1ee6589bd516f876
Evgeniy Nam <ew.nam@yandex.ru>
2024-02-17 14:26:38 +0300

    removed unnecessary comments from the code

--- app/pipline/data_preprocessing/DBAASP_preprocessing.py
+++ app/pipline/data_preprocessing/DBAASP_preprocessing.py
@@ -4,31 +4,11 @@ from app.sequant_tools import SequantTools
 peptides_1 = pd.read_csv('../../utils/data/peptides.csv')
 peptides_2 = pd.read_csv('../../utils/data/peptides_2.csv')
 
-"""
-для DBAASP находим величины связанные с растворимостью (по типу растворимости) 
-с помощью либы peptide + давай около трех величин выберешь сам, 
-соответвенно они не должны быть входящими множествами с дескрипторы rdkit на которых мы учим.
-
-5.1  Сбор сета для DBAASP:
-Объедени оба сета, сброси дубликаты по сиквенсу. Потом идет поиск таргетных величин с помощью либы. 
-Итоговый результат должен состоять из колонок сиквенс + n-таргетов. 
-Далее его нужно сохранить и кинуть в отдельную папку на никитин диск.
-
-Из анализа датасетов известно:
-Среди последовательностейй есть дубликаты, так как активность пептидов измерялась на разных бактериях и разными величинами.
-наиболее многочисленной группой является группа МИК, значит в первую очередь мы сбросим все строки, кроме содержащих МИК
-Далее сгруппируем по таргету и последовательности, чтобы проверить на дубликаты, дубликаты сбросим
-
-Итоговые колонки:
-Секвенс
-Таргет
-Активность по базе данных
-"""
 # Uniting DataFrame
 peptides = pd.concat([peptides_1, peptides_2], axis=0)
 peptides.reset_index(drop=True, inplace=True)
 
-# Keeping only the required columns
+# Keeping only required columns
 needed_columns = ['SEQUENCE', 'TARGET ACTIVITY - TARGET SPECIES',
                   'TARGET ACTIVITY - ACTIVITY MEASURE VALUE',
                   'TARGET ACTIVITY - ACTIVITY (μg/ml) (Calculated By DBAASP)']
